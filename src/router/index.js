310d7f986bd45bc5ede0a85a2ddba83d76a5ebbb
weidongpo <534966250@qq.com>
2026-04-02 10:15:26 +0800

    refactor: 调整views目录结构，每个模块独立文件夹

--- src/router/index.js
+++ src/router/index.js
@@ -4,37 +4,37 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/Home.vue'),
+    component: () => import('@/views/home/index.vue'),
     meta: { title: '首页' }
   },
   {
     path: '/follow',
     name: 'Follow',
-    component: () => import('@/views/Follow.vue'),
+    component: () => import('@/views/follow/index.vue'),
     meta: { title: '追剧' }
   },
   {
     path: '/favorite',
     name: 'Favorite',
-    component: () => import('@/views/Favorite.vue'),
+    component: () => import('@/views/favorite/index.vue'),
     meta: { title: '收藏' }
   },
   {
     path: '/task',
     name: 'Task',
-    component: () => import('@/views/Task.vue'),
+    component: () => import('@/views/task/index.vue'),
     meta: { title: '任务' }
   },
   {
     path: '/profile',
     name: 'Profile',
-    component: () => import('@/views/Profile.vue'),
+    component: () => import('@/views/profile/index.vue'),
     meta: { title: '我的' }
   },
   {
     path: '/detail/:id',
     name: 'Detail',
-    component: () => import('@/views/Detail.vue'),
+    component: () => import('@/views/detail/index.vue'),
     meta: { title: '详情' }
   }
 ]
