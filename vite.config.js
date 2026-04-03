b0e8e65106187c617c7c23d35ef4547f61c199a2
weidongpo <534966250@qq.com>
2026-04-03 19:11:40 +0800

    fix: 修复 vite alias 配置

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 export default defineConfig({
   base: '/drama-h5/',
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src')
     }
   },
   server: {
